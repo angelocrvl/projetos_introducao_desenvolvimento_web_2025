3b57c9b7fc8035b2bef748766f6140fec03f4aaf
angelocrvl <angelocarvalhocruz@gmail.com>
2025-05-20 19:05:23 -0300

    atividade8
    atualização da atividade 8

--- atividade8/script.js
+++ atividade8/script.js
@@ -1,7 +1,7 @@
 
 function calculoTMB() {
     let nome = document.getElementById("nome").value;
-    let sexo = (document.querySelector('input[name="sexo"]:checked').value);
+    let sexo = document.getElementById("sexo").value;
     let TMB = parseFloat;
     let peso = parseInt(document.getElementById("peso").value);
     let altura = parseInt(document.getElementById("altura").value);
@@ -10,7 +10,7 @@ function calculoTMB() {
     let Agua = parseFloat;
     let PerderPeso = parseInt(document.getElementById("pesoP").value);
     let dia = parseInt(document.getElementById("dias").value);
-    let Gperda = (PerderPeso*7700) / dia;
+    let Gperda = (PerderPeso * 7700) / dia;
     let distcaminhada = parseFloat(document.getElementById("dist").value);
     let resultado1 = document.getElementById("resultado1");
     let resultado2 = document.getElementById("resultado2");
@@ -24,7 +24,7 @@ function calculoTMB() {
         resultado2.innerHTML = "o resultado da TMB é: " + TMB;
         resultado3.innerHTML = "o gasto calórico diário total é " + Gcalorico;
         resultado4.innerHTML = "a quantidade ideal de água diária é " + Agua;
-    } else {
+    } if (sexo == "feminino") {
         TMB = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade);
         let Gcalorico = TMB * taxa;
         Agua = 0.035 * peso;
@@ -34,7 +34,7 @@ function calculoTMB() {
         resultado4.innerHTML = "a quantidade ideal de água diária é " + Agua;
     }
 
-    
+
     resultado7.innerHTML = "o déficit calórico necessário para perder " + PerderPeso + "kg em " + dia + "dias é de " + Gperda + "Kcal";
     resultado6.innerHTML = "o gasto calórico numa caminhada de " + distcaminhada + "km " + "é de " + Gcaminhada + "kcal";
     resultado1.innerHTML = "nome do cliente: " + nome;
